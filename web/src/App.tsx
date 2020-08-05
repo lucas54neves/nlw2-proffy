a7e609cd048c58ab7a1a52da1d988109cd92e7a1
Lucas Neves <lucas54neves@gmail.com>
2020-08-05 18:52:50 -0300

    Add first class files

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './assets/styles/global.css';
+import Routers from './routers';
+
+function App() {
+  return (
+    <Routers />
+  );
+}
+
+export default App;
---
+++ web/src/components/teacherItem/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import whatsAppIcon from '../../assets/images/icons/whatsapp.svg';
+import './styles.css';
+
+function TeacherItem() {
+    return (
+        <article className="teacher-item">
+            <header>
+                <img src="https://avatars2.githubusercontent.com/u/2254731?s=400&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4" alt="Diego Fernandes"/>
+                <div>
+                    <strong>Diego Fernandes</strong>
+                    <span>Química</span>
+                </div>
+                <p>
+                    Entusiasta das melhores tecnologias de química avançada.
+                    <br/><br/>
+                    Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+                </p>
+            </header>
+            <footer>
+                <p>
+                    Preço/hora
+                    <strong>R$ 80,00</strong>
+                </p>
+                <button type="button">
+                    <img src={whatsAppIcon} alt="WhatsApp"/>
+                    Entrar em contato
+                </button>
+            </footer>
+        </article>
+    )
+}
+
+export default TeacherItem;
